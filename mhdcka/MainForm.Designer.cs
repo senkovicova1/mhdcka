173b76e7c49a4e774606c98e82b1677ce9f32b66
senkovicova1 <36665069+senkovicova1@users.noreply.github.com>
2019-11-26 20:47:25 +0100

    Initial commit

---
+++ mhdcka/MainForm.Designer.cs
@@ -0,0 +1,106 @@
+﻿
+partial class MainForm
+{
+	private System.ComponentModel.IContainer components = null;
+	private System.Windows.Forms.Button button1;
+	private System.Windows.Forms.Button button2;
+	private System.Windows.Forms.Button button3;
+	private System.Windows.Forms.Button button4;
+	private System.Windows.Forms.Button button5;
+
+	protected override void Dispose(bool disposing)
+	{
+		if (disposing && components != null) 
+		{
+			components.Dispose();
+		}
+		base.Dispose(disposing);
+	}
+
+	private void InitializeComponent()
+	{
+		this.button1 = new System.Windows.Forms.Button();
+		this.button2 = new System.Windows.Forms.Button();
+		this.button3 = new System.Windows.Forms.Button();
+		this.button4 = new System.Windows.Forms.Button();
+		this.button5 = new System.Windows.Forms.Button();
+		this.SuspendLayout();
+		// 
+		// button1
+		// 
+		this.button1.Location = new System.Drawing.Point(8, 8);
+		this.button1.Name = "button1";
+		this.button1.Size = new System.Drawing.Size(160, 25);
+		this.button1.TabIndex = 0;
+		this.button1.Text = "Nová mapa";
+		this.button1.UseVisualStyleBackColor = true;
+		this.button1.Click += new System.EventHandler(this.Button1Click);
+		// 
+		// button2
+		// 
+		this.button2.Location = new System.Drawing.Point(176, 8);
+		this.button2.Name = "button2";
+		this.button2.Size = new System.Drawing.Size(160, 25);
+		this.button2.TabIndex = 1;
+		this.button2.Text = "Načítať mapu";
+		this.button2.UseVisualStyleBackColor = true;
+		this.button2.Click += new System.EventHandler(this.Button2Click);
+		// 
+		// button3
+		// 
+		this.button3.Location = new System.Drawing.Point(344, 8);
+		this.button3.Name = "button3";
+		this.button3.Size = new System.Drawing.Size(160, 25);
+		this.button3.TabIndex = 2;
+		this.button3.Text = "Upraviť mapu";
+		this.button3.UseVisualStyleBackColor = true;
+		this.button3.Click += new System.EventHandler(this.Button3Click);
+		// 
+		// button4
+		// 
+		this.button4.Location = new System.Drawing.Point(512, 8);
+		this.button4.Name = "button4";
+		this.button4.Size = new System.Drawing.Size(160, 25);
+		this.button4.TabIndex = 3;
+		this.button4.Text = "Uložiť mapu";
+		this.button4.UseVisualStyleBackColor = true;
+		this.button4.Click += new System.EventHandler(this.Button4Click);
+		// 
+		// button5
+		// 
+		this.button5.Location = new System.Drawing.Point(680, 8);
+		this.button5.Name = "button5";
+		this.button5.Size = new System.Drawing.Size(160, 25);
+		this.button5.TabIndex = 4;
+		this.button5.Text = "Generovanie otázok";
+		this.button5.UseVisualStyleBackColor = true;
+		this.button5.Click += new System.EventHandler(this.Button5Click);
+		// 
+		// MainForm
+		// 
+		this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+		this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+		this.ClientSize = new System.Drawing.Size(849, 562);
+		this.Controls.Add(this.button5);
+		this.Controls.Add(this.button4);
+		this.Controls.Add(this.button3);
+		this.Controls.Add(this.button2);
+		this.Controls.Add(this.button1);
+		this.DoubleBuffered = true;
+		this.Name = "MainForm";
+		this.Text = "mhdcka";
+		this.Load += new System.EventHandler(this.MainFormLoad);
+		this.Paint += new System.Windows.Forms.PaintEventHandler(this.MainFormPaint);
+		this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.MainFormMouseClick);
+		this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MainFormMouseDown);
+		this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MainFormMouseMove);
+		this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.MainFormMouseUp);
+		this.ResumeLayout(false);
+
+	}
+
+	public MainForm()
+	{
+		InitializeComponent();
+	}
+}
---
+++ mhdcka/MainForm.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.IO;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+partial class MainForm : Form
+{
+
+	static SolidBrush white = new SolidBrush(Color.White);
+	static Pen zastavkaPen = new Pen(Color.MediumSlateBlue);
+	static SolidBrush zastavkaBrush = new SolidBrush(Color.AliceBlue);
+	
+	static List<Zastavka> zastavky;
+	static List<Linka> linky;
+	
+	static int rezim = 1;
+	
+	static bool holding = false;
+	
+	static int newLineX1, newLineY1, newLineX2, newLineY2;
+	static Zastavka startZast;
+
+	void Button1Click(object sender, EventArgs e)
+	{
+		rezim = 1;
+	}
+	void Button2Click(object sender, EventArgs e)
+	{
+		rezim = 2;
+	}
+	void Button3Click(object sender, EventArgs e)
+	{
+		rezim = 3;
+	}
+	void Button4Click(object sender, EventArgs e)
+	{
+		rezim = 4;
+	}
+	void Button5Click(object sender, EventArgs e)
+	{
+		rezim = 5;
+	}
+	
+	void MainFormPaint(object sender, PaintEventArgs e)
+	{
+		Graphics g = e.Graphics;
+		g.FillRectangle(white, 0,0, 865,600);
+		
+		if (newLineX1 > -1){
+			g.DrawLine(zastavkaPen, newLineX1, newLineY1, newLineX2, newLineY2);
+		}
+		
+		foreach (var l in linky){
+			l.kresli(g);
+		}
+		
+		foreach (var z in zastavky)
+		{
+			z.kresli(g);
+		}
+	}
+	
+	void MainFormLoad(object sender, EventArgs e)
+	{
+		zastavky = new List<Zastavka>();
+		linky = new List<Linka>();
+		zastavkaPen.Width = 5;
+	}
+	
+	void MainFormMouseClick(object sender, MouseEventArgs e)
+	{
+		if (rezim == 3 && closeTo(e.X, e.Y) == null && !holding){
+			zastavky.Add(new Zastavka(e.X, e.Y));
+			Invalidate();
+			Update();
+		}
+
+	}
+	
+	void MainFormMouseMove(object sender, MouseEventArgs e)
+	{
+		if (holding){
+			newLineX2 = e.X;
+			newLineY2 = e.Y;
+			Invalidate();
+			Update();
+		}
+	}
+	
+	void MainFormMouseDown(object sender, MouseEventArgs e)
+	{				
+		Zastavka z = closeTo(e.X, e.Y);
+		if (rezim == 3 && z != null){
+			holding = true;
+			startZast = z;
+			newLineX1 = z.X;
+			newLineY1 = z.Y;
+			newLineX2 = e.X;
+			newLineY2 = e.Y;
+			Invalidate();
+			Update();
+		}
+	}
+	
+	void MainFormMouseUp(object sender, MouseEventArgs e)
+	{	
+		if (holding){
+			Zastavka z = closeTo(e.X, e.Y);
+			if (z != null && z.X != startZast.X && z.Y != startZast.Y){
+				spoj(z, startZast);
+				Linka newLinka = new Linka(startZast, z);
+				linky.Add(newLinka);
+			} 
+			newLineX1 = -1;
+			newLineX2 = -1;
+			newLineY1 = -1;
+			newLineY2 = -1;
+			
+			Invalidate();
+			Update();
+			holding = false;
+		}
+	}
+	
+	void spoj(Zastavka z1, Zastavka z2){
+		z1.spoj(z2);
+		z2.spoj(z1);
+	}
+	
+	bool clickedZast(int X, int Y){
+		return true;
+	}
+			
+	Zastavka closeTo(int X, int Y){
+	    foreach (var z in zastavky)
+	    {
+	    	if (z.isClose(X, Y)){
+	    		return z;
+	    	}
+	    }
+		return null;
+	}
+
+	class Linka {		
+		List<Zastavka> zastavky;
+		public string name;
+		public Pen colour;
+		
+		public Linka(Zastavka z1, Zastavka z2){
+			zastavky = new List<Zastavka>();
+			zastavky.Add(z1);
+			zastavky.Add(z2);
+			colour = zastavkaPen;
+		}
+		
+		public void kresli(Graphics g){
+			for (int i = 0; i < zastavky.Count - 1; i++)
+			{
+				g.DrawLine(colour, zastavky[i].X, zastavky[i].Y, zastavky[i+1].X, zastavky[i+1].Y);
+			} 
+			
+		}
+		
+	}
+	
+	class Zastavka {
+	
+		public int X, Y, r;
+		string name;
+		public Dictionary<Zastavka, int> susedia;
+	
+		public Zastavka(int nX, int nY){
+			susedia = new Dictionary<Zastavka, int>();
+			X = nX;			
+			Y = nY;
+			r = 10;
+		}		
+		
+		public void kresli(Graphics g){
+			g.DrawEllipse(zastavkaPen, X-r, Y-r, 2*r, 2*r);
+			g.FillEllipse(zastavkaBrush, X-r, Y-r, 2*r, 2*r);
+		}
+		
+		public bool isClose(int X, int Y){
+			return Math.Sqrt((this.X - X)*(this.X - X) + (this.Y - Y)*(this.Y - Y)) < (r + 10 + 20);
+		}
+		
+		public void spoj(Zastavka z){
+			if (!susedia.ContainsKey(z)){
+				susedia[z] = 1;
+			} else {
+				susedia[z] += 1;
+			}
+			
+			foreach (var value in susedia.Values)
+			{
+				if (10 + 5 * (value - 1) > r){
+					r = 10 + 5 * (value - 1);
+				}
+			}
+		}
+			
+	}
+}
